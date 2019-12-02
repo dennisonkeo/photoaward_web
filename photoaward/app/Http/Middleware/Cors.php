aa34147d4bce23966268a1a84dd081bcb0be1ed1
fadeit <jomoreit@gmail.com>
2019-12-02 18:50:46 +0300

    modified cors middleware

--- photoaward/app/Http/Middleware/Cors.php
+++ photoaward/app/Http/Middleware/Cors.php
@@ -15,24 +15,27 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
+        // header("Access-Control-Allow-Origin: *");
 
-        $headers = [
-            'Access-Control-Allow-Methods'=>'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers'=>'Content-Type,X-Auth-Token,Origin,Authorization',
-        ];
+        // $headers = [
+        //     'Access-Control-Allow-Methods'=>'POST,GET,OPTIONS,PUT,DELETE',
+        //     'Access-Control-Allow-Headers'=>'Content-Type,X-Auth-Token,Origin,Authorization',
+        // ];
 
-        if($request->getMethod() == "OPTIONS")
-        {
-            return response()->json('OK',200,$headers);
+        // if($request->getMethod() == "OPTIONS")
+        // {
+        //     return response()->json('OK',200,$headers);
+        // }
+
+        // $response = $next($request);
+        // foreach($headers as $key => $value)
+        // {
+        //     $response->header($key, $value);
         }
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
-
-        return $response;
+        return $next($request)
+        ->header('Access-Control-Allow-Origin',"*")
+        ->header('Access-Control-Allow-Methods',"POST,GET,OPTIONS,PUT,DELETE")
+        ->header('Access-Control-Allow-Headers',"Accept,Content-Type,Authorization");
     }
 }
