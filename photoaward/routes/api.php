a89e5c26fcb7f2b54dd23bc71114a16cafe35e43
Dennis <dennis.onkeo81@gmail.com>
2019-12-03 16:51:37 +0300

    added MobileAppApi controller and passport

--- photoaward/routes/api.php
+++ photoaward/routes/api.php
@@ -17,11 +17,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('album', 'HomeController@album')->name('album');
+
+Route::post('login', 'MobileAppController@login');
+
+Route::get('check_login', 'MobileAppController@user_loggedin');
+
+Route::get('likes/{id}', 'MobileAppController@get_rate_count');
+
+Route::group(['middleware' => 'auth:api'], function(){
+
 Route::post('mpesa-response', 'PaymentController@mpesa_response')->name('mpesa-response');
 
 Route::post('mpesa-reply', 'CartController@mpesa_reply')->name('mpesa-reply');
 
+});
 
-Route::get('album', 'HomeController@album')->name('album');
 
 
