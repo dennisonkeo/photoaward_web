97a91c0d510477b383a8293c620715dc5e1cdcf1
Dennis <dennis.onkeo81@gmail.com>
2019-12-04 10:25:55 +0300

    modified register blade

--- photoaward/resources/views/register.blade.php
+++ photoaward/resources/views/register.blade.php
@@ -217,7 +217,7 @@
 					<div class="container-login100-form-btn" >
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-							<button type="submit" class="login100-form-btn">
+							<button class="login100-form-btn" type="submit" >
 								Sign up
 							</button>
 						</div>
