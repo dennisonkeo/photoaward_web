8b46c52663a47aac6081ebf2f42b9a555145445f
Dennis <dennis.onkeo81@gmail.com>
2020-01-06 13:30:15 +0300

    modified kernel

--- photoaward/app/Console/Commands/DeleteCron.php
+++ photoaward/app/Console/Commands/DeleteCron.php
@@ -3,27 +3,27 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Notifications\HelloUser;
+
 use Auth;
 use App\Upload;
 use Illuminate\Support\Facades\File;
 use Image;
 
-class DeleteUploads extends Command
+class DeleteCron extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'delete:uploads';
+    protected $signature = 'delete:cron';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This command deletes unpaid uploads after 12 hours elapses';
+    protected $description = 'Command to delete unpaid uploads when 12 hours elapses';
 
     /**
      * Create a new command instance.
@@ -42,15 +42,25 @@ class DeleteUploads extends Command
      */
     public function handle()
     {
-      //   $user = Auth::user();
-
-      // $user->notify(new HelloUser());
-
-      $upload =Upload::where('id',2)->first();
+        $upload =Upload::where('id',4)->first();
 
       $upload->delete();
       File::delete([public_path($upload->imagePath)]);
-        
 
+                \Log::info("Cron is working fine!");
+
+     
+
+        /*
+
+           Write your database logic we bellow:
+
+           Item::create(['name'=>'hello new']);
+
+        */
+
+      
+
+        $this->info('Demo:Cron Cummand Run successfully!');
     }
 }
