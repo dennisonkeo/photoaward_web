5278ffb83f480308d1d5d74345abd7a46d261153
Dennis <dennis.onkeo81@gmail.com>
2020-08-11 16:06:31 +0300

    modified payment controller

--- photoaward/app/Http/Controllers/PaymentController.php
+++ photoaward/app/Http/Controllers/PaymentController.php
@@ -54,43 +54,60 @@ class PaymentController extends Controller
 		$BusinessShortCode = "523608";
 		$LipaNaMpesaPasskey = "78dbd4c3ecda6503b00be053264fe0760ae70f7c5c0c3c6f49869fbc5ccdb346";
 		$TransactionType = "CustomerPayBillOnline";
-		$Amount = "1";
+		$Amount = $total_amount;
 		$PartyA = Auth::user()->phone;
 		$PartyB = "523608";
 		$PhoneNumber = Auth::user()->phone;
-		$CallBackURL = 'https://034fa128.ngrok.io/api/mpesa-response';
+		$CallBackURL = 'https://picture254.com/api/mpesa-response';
 		$AccountReference = Auth::user()->phone;
 		$TransactionDesc = "Payment";
-		$Remarks = "Yess 
-	        	   $pay = new Payment();
+		$Remarks = "Yess" ;
+
+			$BusinessShortCode = "523608";
+          $LipaNaMpesaPasskey = "NTIzNjA4NzhkYmQ0YzNlY2RhNjUwM2IwMGJlMDUzMjY0ZmUwNzYwYWU3MGY3YzVjMGMzYzZmNDk4NjlmYmM1Y2NkYjM0NjIwMTkxMTE4MTUzMzQ4";
+          $TransactionType = "CustomerPayBillOnline";
+          $Amount = '1';
+          $PartyA = Auth::user()->phone;
+          $PartyB = "523608";
+          $PhoneNumber = Auth::user()->phone;
+          $CallBackURL = 'http://picture254.com/api/mpesa-reply';
+          $AccountReference = Auth::user()->phone;
+          $TransactionDesc = "Payment";
+          $Remarks = "Yess";
+
+          $stkPushSimulation = $mpesa->STKPushSimulation($BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks
+                                  );
+                // return $stkPushSimulation;
+          // $check = $stkPushSimulation; 
+	      //   	   $pay = new Payment();
 				             
-				   $pay->phone = $phone;
-				   $pay->trans_no = $trans_no;
-				   $pay->account_no = $account_no;
-				   $pay->trans_date = $trans_date;
-				   $pay->amount = $amount;
+				   // $pay->phone = Auth::user()->phone;
+				   // $pay->trans_no = $trans_no;
+				   // $pay->account_no = $account_no;
+				   // $pay->trans_date = $trans_date;
+				   // $pay->amount = $amount;
 
 				             
-				    $pay->save();
+				   //  $pay->save();
 
-				    Upload::where('user_id', Auth::user()->id)
-			                                ->where('uploaded','no')
-			                                ->update(array('uploaded' => 'yes'));
+				    // Upload::where('user_id', Auth::user()->id)
+			     //                            ->where('uploaded','no')
+			     //                            ->update(array('uploaded' => 'yes'));
 				             
-				    ImagePay::where('account_no', $account_no)
-				             ->update(array('status' => 'Paid'));
+				    // ImagePay::where('account_no', $account_no)
+				    //          ->update(array('status' => 'Paid'));
 
 				return response()->json('Success');
-			                           $upload->account_no = $check;
+			                           // $upload->account_no = $check;
 			             
-			                           $upload->save();
+			                           // $upload->save();
 			             
 			                        // Upload::where('user_id', Auth::user()->id)
 			                        //         ->where('uploaded','no')
 			                        //         ->update(array('uploaded' => 'yes'));
 
 			                        return response()->json('0');
-		}
+		
 	}
 
 	public function mpesa_response(Request $request)
